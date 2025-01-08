4bfe6328ea6b2061efbf48aeea0c68be119f87aa
John Oyler <john.m.oyler@gmail.com>
2025-01-08 01:53:43 -0600

    Another circular scraper (start).

---
+++ circulars/retrieve_lidl_uk.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import requests, tempfile
+
+################################################################################
+
+region = '517240'  
+file_location = './pdfs/' # Create (or change) directory or script will fail.
+
+################################################################################
+
+# https://www.lidl.co.uk/l/en/online-leaflets/02-01-08-01-england-leaflet/ar/0
+# https://www.lidl.co.uk/l/en/online-leaflets/02-01-08-01-lidl-weekly/ar/14
